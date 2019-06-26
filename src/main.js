89f9ead99bf3d1a62ad58c60b022b1448f2653f8
LucasDMS <lucasdelmondes14@live.com>
2019-06-26 12:30:46 -0300

    Finished

--- src/main.js
+++ src/main.js
@@ -1,27 +1,87 @@
+import api from './api'
+
 class App{
     constructor(){
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
-
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl = document.getElementById('repo-list');
         this.registerHandlers();
     }
 
     registerHandlers(){
         this.formEl.onsubmit = event => this.addRepository(event)
     }
+
+    setLoading(loading = true){
+        if (loading === true){
+            let loadingEl = document.createElement('span');
+            loadingEl.appendChild(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id', 'loading');
+            this.formEl.appendChild(loadingEl);
+        } else {
+            document.getElementById('loading').remove();
+        }
+    }
     
-    addRepository(event){
+    async addRepository(event){
         event.preventDefault();
         
-        this.repositories.push({
-            name: 'BBBB',
-            description: 'AAAA',
-            avatar_url: 'https://avatars2.githubusercontent.com/u/38070771?v=4',
-            html_url: 'https://github.com/LucasDMS/Example-ES6',
-        });
-        console.log(this.repositories);
-        
+        const repoInput = this.inputEl.value;
+
+        if(repoInput.length === 0)
+            return;
+
+        this.setLoading();
+
+        try{
+            const response = await api.get(`repos/${repoInput}`);
+
+            const { name, description , html_url,  owner: { avatar_url } } = response.data;
+
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+            this.inputEl.value = '';
+            
+            this.render();
+        }catch(E){
+            alert('Repositorio não existe!')
+        }
+        this.setLoading(false);
+    }
+    render(){
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo =>{
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.setAttribute('href', repo.html_url);
+            linkEl.appendChild(document.createTextNode('Acessar'));
+            
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+            console.log(listItemEl);
+            
+            this.listEl.appendChild(listItemEl);
+
+        })
     }
 }
 
